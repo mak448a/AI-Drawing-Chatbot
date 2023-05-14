54244b69b6d8c6e2aee9bbffb1edd13f1e9743c0
mak448a <mak448a@gmail.com>
2023-05-14 16:50:11 -0500

    Fix bug introduced in last commit

--- bot.py
+++ bot.py
@@ -44,10 +44,10 @@ async def imagine(interaction: discord.Interaction, prompt: str):
     current_time = time.time()
 
     if platform.system() == "Windows":
-        os.system(f"python AI-Horde/cli_request.py --prompt '{sanitized}'"
+        os.system(f"python AI-Horde-With-Cli/cli_request.py --prompt '{sanitized}'"
                   f" --api_key '{api_key}' -n 4 -f {current_time}.png")
     else:
-        os.system(f"python3 AI-Horde/cli_request.py --prompt '{sanitized}'"
+        os.system(f"python3 AI-Horde-With-Cli/cli_request.py --prompt '{sanitized}'"
                   f" --api_key '{api_key}' -n 4 -f {current_time}.png")
 
     # Loop until image generates
