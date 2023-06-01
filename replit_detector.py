2aad5dffa2b5e524f5bca3769ed758b41274b8cf
mak448a <mak448a@gmail.com>
2023-06-01 17:07:45 -0500

    Update replit_detector.py

--- replit_detector.py
+++ replit_detector.py
@@ -1,5 +1,6 @@
 from os import environ
 
+# Solution from https://replit.com/talk/ask/Can-a-Python-script-detect-it-is-running-in-replit/24945
 if "REPL_OWNER" in environ:
     is_replit = True
 else:
