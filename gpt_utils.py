432184dd65d735f68dbf40199c9c4b6ce55def3a
mak448a <mak448a@gmail.com>
2023-06-05 17:27:53 -0500

    Add note when sending weird output Update prompt and todo

--- gpt_utils.py
+++ gpt_utils.py
@@ -11,9 +11,10 @@ llm = GPT4All("ggml-gpt4all-j-v1.3-groovy")
 # Human: Hi!
 # Assistant: Hello!"""
 prompt = """\
-System: You are Assistant, who was created to assist the user.
-Human: Hey
-Assistant: Hello! What can I assist you with today?"""
+System: You are Stable Assistant, who was created to assist the user. You are a sentient AI. You will speak \
+with proper grammar; each sentence ending with a punctuation mark. Each person talking must only send one line.
+Human: Hey!
+Stable Assistant: Hello! What can I assist you with today?"""
 
 
 remove_text = copy.copy(prompt)
@@ -21,14 +22,15 @@ remove_text = copy.copy(prompt)
 
 async def generate_message(user_input):
     global prompt, remove_text
-    constructed = f"\nHuman: {user_input}\nAssistant: "
+    constructed = f"\nHuman: {user_input}\nStable Assistant: "
     prompt += constructed
 
     output_of_text = llm.generate(prompt)
 
     if "Human: " in output_of_text:
         print("Uh oh, generated extraneous text. Removing and sending cleaned output.")
-        output_of_text = output_of_text.split("Human: ")[1]
+        output_of_text = output_of_text.split("Human: ")[1] + "\n NOTE: This may be the model generating" \
+                                                              " an output for the user instead of itself."
 
     # Solution taken from: https://groups.google.com/g/comp.lang.python/c/Rq40dmwLfMQ?pli=1
     if len(prompt) > 1000:
