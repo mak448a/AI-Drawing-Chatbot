7526298a3f12694720d8e78f0621363deeab939b
mak448a <mak448a@gmail.com>
2023-06-04 20:55:10 -0500

    Organize image functions into a new file Removed unnecessary requirements Removed code that didn't need to be there

---
+++ image_generation_utils.py
@@ -0,0 +1,68 @@
+from horde_module import Generator
+from utils import api_key
+import uuid
+import asyncio
+import os
+import discord
+from imaginepy import AsyncImagine, Style, Ratio
+
+horde_generator = Generator()
+
+
+async def generate_with_stable_horde(prompt, use_anything_diffusion, ctx):
+    await ctx.send(f"{ctx.message.author.mention} is generating \"{prompt}\" with "
+                   f"{'Anything Diffusion' if use_anything_diffusion else 'Stable Diffusion'}")
+
+    print(f"{ctx.message.author.name} is generating \"{prompt}\" with "
+          f"{'Anything Diffusion' if use_anything_diffusion else 'Stable Diffusion'}")
+
+    file_uuid = uuid.uuid1()
+    await horde_generator.generate(prompt, api_key, f"{file_uuid}.png", 4,
+                                   f"{'Anything Diffusion' if use_anything_diffusion else 'stable_diffusion_2.1'}")
+
+    # Loop until the images generate. We check for the fourth image.
+    while True:
+        if os.path.exists(f"3_{file_uuid}.png"):
+            break
+        await asyncio.sleep(0.8)
+
+    images = []
+
+    # Grab all the filenames
+    for i in range(4):
+        images.append(f"{i}_{file_uuid}.png")
+
+    image_files = [discord.File(image) for image in images]
+
+    await ctx.send(files=image_files)
+
+    # Remove all the files
+    for i in range(4):
+        os.remove(f"{i}_{file_uuid}.png")
+
+
+# Imaginepy function
+async def generate_image_with_imaginepy(image_prompt, style_value, ratio_value):
+    async_imagine = AsyncImagine()
+    filename = str(uuid.uuid4()) + ".png"
+    style_enum = Style[style_value]
+    ratio_enum = Ratio[ratio_value]
+    img_data = await async_imagine.sdprem(
+        prompt=image_prompt,
+        style=style_enum,
+        ratio=ratio_enum
+    )
+    if img_data is None:
+        print("An error occurred while generating the image.")
+        return
+
+    try:
+        with open(filename, mode="wb") as img_file:
+            img_file.write(img_data)
+    except Exception as e:
+        print(f"An error occurred while writing the image to file: {e}")
+        return None
+
+    await async_imagine.close()
+
+    return filename
--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from utils import api_key, bot_token
+from utils import bot_token
 from gpt_utils import generate_message
 from replit_detector import is_replit
-from horde_module import Generator
+from image_generation_utils import generate_with_stable_horde, generate_image_with_imaginepy
+
 import discord
 from discord.ext import commands
 from discord import app_commands
 
-import asyncio
-import random
-import uuid
-import os
-
-from imaginepy import AsyncImagine, Style, Ratio
 import urllib.parse
 import requests
-import httpx
+
+import random
+import os
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
-horde_generator = Generator()
 
 
 @bot.event
@@ -54,72 +51,6 @@ async def on_message(message):
                 await message.channel.send(msg)
         except:  # NOQA
             await message.channel.send("I had an error.")
-
-
-# Imaginepy function
-async def generate_image_with_imaginepy(image_prompt, style_value, ratio_value):
-    async_imagine = AsyncImagine()
-    filename = str(uuid.uuid4()) + ".png"
-    style_enum = Style[style_value]
-    ratio_enum = Ratio[ratio_value]
-    img_data = await async_imagine.sdprem(
-        prompt=image_prompt,
-        style=style_enum,
-        ratio=ratio_enum
-    )
-    if img_data is None:
-        print("An error occurred while generating the image.")
-        return
-
-    try:
-        with open(filename, mode="wb") as img_file:
-            img_file.write(img_data)
-    except Exception as e:
-        print(f"An error occurred while writing the image to file: {e}")
-        return None
-
-    await async_imagine.close()
-
-    return filename
-
-
-async def download_imaginepy_image(image_url, save_as):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(image_url)
-    with open(save_as, "wb") as f:
-        f.write(response.content)
-
-
-async def generate_with_stable_horde(prompt, use_anything_diffusion, ctx):
-    await ctx.send(f"{ctx.message.author.mention} is generating \"{prompt}\" with "
-                   f"{'Anything Diffusion' if use_anything_diffusion else 'Stable Diffusion'}")
-
-    print(f"{ctx.message.author.name} is generating \"{prompt}\" with "
-          f"{'Anything Diffusion' if use_anything_diffusion else 'Stable Diffusion'}")
-
-    file_uuid = uuid.uuid1()
-    await horde_generator.generate(prompt, api_key, f"{file_uuid}.png", 4,
-                                   f"{'Anything Diffusion' if use_anything_diffusion else 'stable_diffusion_2.1'}")
-
-    # Loop until the images generate. We check for the fourth image.
-    while True:
-        if os.path.exists(f"3_{file_uuid}.png"):
-            break
-        await asyncio.sleep(0.8)
-
-    images = []
-
-    # Grab all the filenames
-    for i in range(4):
-        images.append(f"{i}_{file_uuid}.png")
-
-    image_files = [discord.File(image) for image in images]
-
-    await ctx.send(files=image_files)
-
-    # Remove all the files
-    for i in range(4):
-        os.remove(f"{i}_{file_uuid}.png")
 
 
 @bot.hybrid_command(name="imagine", description="Generate an image with Stable Diffusion")
