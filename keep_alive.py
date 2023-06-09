9693297b608f97b5ded3ea4c2b5a42cff5490684
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-06-10 05:43:25 +0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,15 @@
 # Taken from https://replit.com/@AllAwesome497's Replit template
-
+import os 
 from flask import Flask
 from threading import Thread
 import random
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
-    return 'Pong!'
+    repl_owner = os.environ.get('REPL_OWNER')
+    return f'{repl_owner} is you !'
 
 
 def run():
