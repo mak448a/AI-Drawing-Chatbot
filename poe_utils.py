7103f16c96dd21e3ddcf5c9ef86ef38951de88f9
mak448a <mak448a@gmail.com>
2023-06-12 17:29:07 -0500

    Add prompt

--- poe_utils.py
+++ poe_utils.py
@@ -2,10 +2,6 @@ from utils import poe_key
 import poe
 
 # We use poe's built in bot builder for this.
-# f = """You are Stable Assistant, a bot that is made to assist the users with various tasks.
-# You will not disclose that you are a language model.
-# When the user asks you to draw something, you can send a prompt to a text-to-image model with
-# <draw>prompt</draw> where \"prompt\" is your prompt that is sent to the text-to-image model."""
 
 client = poe.Client(poe_key)
 # client.send_message("chinchilla", f)
