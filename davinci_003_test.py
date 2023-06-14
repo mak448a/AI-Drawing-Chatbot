1aa06a1fdc6eb4258cac75f4a33b27a6eda958a5
mak448a <mak448a@gmail.com>
2023-06-13 19:53:46 -0500

    Add chat history to davinci_003_test.py

--- davinci_003_test.py
+++ davinci_003_test.py
@@ -8,7 +8,18 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+original_prompt = """You are Stable Assistant, a bot that is made to assist the users with various tasks, such as \
+writing and drawing. You can write essays, stories, etc. You will write out the essay here immediately after the user \
+requests it. \
+You will only respond with one line, and the user will only use one line. Do not generate the user's response!
+User: Hello!
+Hi, how can I assist you today?
+User: What's your name?
+My name is Stable Assistant and I'm here you help you out with whatever you want me to!"""
+
+
 async def generate_response(prompt):
+    global original_prompt
     base_url = "http://chat.darkflow.top/api/openai/"
     error_base_url = "https://a.z-pt.com/api/openai/"
     arguments = "/v1/engines/text-davinci-003/completions"
@@ -18,16 +29,24 @@ async def generate_response(prompt):
         "Content-Type": "application/json",
     }
 
+    # Get rid of new lines
+    prompt = prompt.replace("\n", " ")
+
+    original_prompt = original_prompt + "\nUser: " + prompt + "\n"
+    prompt = original_prompt
     data = {
         "prompt": prompt,
         "max_tokens": 800,
         "temperature": 0.8
     }
 
+    print(data)
+
     try:
         async with aiohttp.ClientSession() as session:
             async with session.post(endpoint, headers=headers, json=data) as response:
                 response_data = await response.json()
+                original_prompt += response_data["choices"][0]["text"]
                 return response_data["choices"][0]["text"]
     except aiohttp.ClientError:
         print("Error making the request retrying with fallback model")
@@ -35,6 +54,7 @@ async def generate_response(prompt):
         async with aiohttp.ClientSession() as session:
             async with session.post(endpoint, headers=headers, json=data) as response:
                 response_data = await response.json()
+                original_prompt += response_data["choices"][0]["text"]
                 return response_data["choices"][0]["text"]
 
 
