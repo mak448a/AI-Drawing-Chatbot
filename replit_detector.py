453f2733d4e248d3df7936b80e1c5474abae6123
mak448a <mak448a@gmail.com>
2023-06-21 15:18:39 -0500

    Make replit_detector.py use logging #50

--- replit_detector.py
+++ replit_detector.py
@@ -1,4 +1,5 @@
 from os import environ
+import logging
 
 # Solution from https://replit.com/talk/ask/Can-a-Python-script-detect-it-is-running-in-replit/24945
 if "REPL_OWNER" in environ:
@@ -6,4 +7,4 @@ if "REPL_OWNER" in environ:
 else:
     is_replit = False
 
-print("We're on Replit!" if is_replit else "We're not on Replit.")
+logging.info("We're on Replit!" if is_replit else "We're not on Replit.")
