d661a157d2795c4b0d6e4a8ff734a9b78c57e52e
mak448a <mak448a@gmail.com>
2023-07-09 21:13:04 -0500

    Add run/setup script for Linux based systems

---
+++ run.sh
@@ -0,0 +1,23 @@
+FILE=venv/bin/activate
+if test -f "$FILE";
+then
+  echo "Already setup! Let's get running!"
+  source venv/bin/activate
+  python3 main.py
+else
+  cp "example_config.json" "config.json"
+  touch .env
+  python3 -m venv venv
+  source venv/bin/activate
+  pip install -r requirements.txt
+  echo -n "Enter your bot token: "
+  read TOKEN
+  echo "BOT_TOKEN=$TOKEN" >> .env
+  echo -n "Enter your Poe token: "
+  read POE_TOKEN
+  echo "POE_TOKEN=$POE_TOKEN" >> .env
+  echo -n "Enter your API Key: "
+  read API_KEY
+  echo "API_KEY=$API_KEY" >> .env
+  python3 main.py
+fi
